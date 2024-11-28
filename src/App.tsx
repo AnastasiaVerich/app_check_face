f599c703e33eab3a34c7b4b19bae3bcf124b2166
veric <verich@mailru>
2024-11-28 11:49:56 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 const isDev = false
-const URL_= isDev?'http://localhost:5000/': 'https://green-apple.io/'
+//const URL_= isDev?'http://localhost:5000/': 'https://green-apple.io/'
 // Функция для отправки запроса
 const fetch_request = async (url: string, method: 'POST' | 'GET', body: FormData, setError: any) => {
     try {
-        const response = await fetch(`${URL_}${url}`, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}${url}`, {
             method: method,
             body: body, // Отправляем FormData
         });
