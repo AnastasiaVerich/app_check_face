e8b0ac7491168d27f610bbec716a46a7918b3862
veric <verich@mailru>
2024-12-25 15:22:09 +0300

    1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -194,7 +194,7 @@ function App() {
                     <HStack gap={'10'} max>
                         <Svg Svg={CameraSvg}/>
                         <Text
-                            text={ON_CAMERA+1}
+                            text={ON_CAMERA+2}
                             type={'text'}
                             max
                         />
