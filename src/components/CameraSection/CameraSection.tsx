e1bc4d49140606c82f5a3d74925a5e943b04e420
veric <verich@mailru>
2025-05-23 15:51:31 +0300

    1

--- src/components/CameraSection/CameraSection.tsx
+++ src/components/CameraSection/CameraSection.tsx
@@ -142,7 +142,6 @@ const CameraSection: React.FC<CameraProps> = ({
                     ref={videoRef}
                     playsInline
                     webkit-playsinline="true"
-                    autoPlay
                     muted
                     controls={false}
                     onLoadedMetadata={() => console.log(`onLoadedMetadata: ${new Date().toLocaleTimeString()}`)}
