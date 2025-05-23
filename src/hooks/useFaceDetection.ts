070fd706580cc572caf0c843e71f59c35f91ae41
veric <verich@mailru>
2025-05-23 17:59:43 +0300

    1

--- src/hooks/useFaceDetection.ts
+++ src/hooks/useFaceDetection.ts
@@ -28,98 +28,107 @@ export const useFaceDetection = (
 
             setModelsLoaded(true);
         };
-        if(isLoaded){
+        if (isLoaded) {
             loadModels();
         }
     }, [isLoaded]);
 
     // Детекция лиц
     useEffect(() => {
-        let interval: any
-        // Если модели загружены и камера включена
+        let timeoutId: any = null;        // Если модели загружены и камера включена
         if (modelsLoaded && isCameraOn && isLoaded) {
-            let y = 1
+            let y = 0
             const detectFace = async () => {
+                y++
                 const i = y
-                if (videoRef.current && canvasRef.current && videoBorderRef.current) { // Если video и canvas элементы существуют
-                    console.log(i+') 1', new Date().toISOString());
+                try {
+                    if (videoRef.current && canvasRef.current && videoBorderRef.current) { // Если video и canvas элементы существуют
+                        console.log(i + ') 1', new Date().toISOString());
 
-                    const video = videoRef.current;
-                    const canvas = canvasRef.current;
-                    const video_border = videoBorderRef.current;
+                        const video = videoRef.current;
+                        const canvas = canvasRef.current;
+                        const video_border = videoBorderRef.current;
 
-                    // Настроим размеры канваса, чтобы он соответствовал размеру видео
-                    const displaySize = {
-                        width: video_border.offsetWidth,
-                        height: video_border.offsetHeight
-                    };
-                    // Настроим канвас для масштабирования детекций
-                    matchDimensions(canvas, displaySize);
-                    console.log(i+') 2', new Date().toISOString());
+                        // Настроим размеры канваса, чтобы он соответствовал размеру видео
+                        const displaySize = {
+                            width: video_border.offsetWidth,
+                            height: video_border.offsetHeight
+                        };
+                        // Настроим канвас для масштабирования детекций
+                        matchDimensions(canvas, displaySize);
+                        console.log(i + ') 2', new Date().toISOString());
 
-                    // Детекция всех лиц с использованием опций для детектора
-                    const options = new TinyFaceDetectorOptions({inputSize: 416, scoreThreshold: 0.3});
-                    const detections = await detectAllFaces(video, options); // Получаем все обнаруженные лица на видео
-                    const resizedDetections = resizeResults(detections, displaySize); // Масштабируем результаты детекции под размер видео
-                    console.log(i+') 3', new Date().toISOString());
+                        // Детекция всех лиц с использованием опций для детектора
+                        const options = new TinyFaceDetectorOptions({inputSize: 416, scoreThreshold: 0.3});
+                        const detections = await detectAllFaces(video, options); // Получаем все обнаруженные лица на видео
+                        const resizedDetections = resizeResults(detections, displaySize); // Масштабируем результаты детекции под размер видео
+                        console.log(i + ') 3', new Date().toISOString());
 
-                    let ctx: any = null
-                    if (isDraw) {
-                        ctx = canvas.getContext('2d');
-                        if (!ctx) return
-                        ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    }
-
-
-                    // Проверяем, попадает ли лицо в оверлей
-                    const circle: Circle = {cx: canvas.clientWidth / 2, cy: canvas.clientHeight / 2, r: 150}; // Создаем круг в центре видео с радиусом 150px
-
-                    let faceDetected = false// Флаг для проверки, было ли найдено лицо в области круга
-                    resizedDetections.forEach((detection) => {
-                            // Извлекаем информацию о прямоугольнике, в котором расположено лицо
-                            const {x, y, width, height} = detection.box;
-                            // Если применен scaleX(-1), нужно скорректировать координаты
-                            const transformedX = canvas.clientWidth - (x + width);
-                            //const rectangle: Rectangle = {x: transformedX, y, width, height};
-                            const rectangle: Rectangle = {x: transformedX, y, width, height}
-
-                            // Проверяем, перекрывает ли прямоугольник (лицо) круг более чем на 80%
-                            // И что бы круг был не больше чем в 5 раз больше прямоугольник , инчае слишко далеко лицо
-                            faceDetected = isRectangleCoveredByCircle(circle, rectangle, 0.8)
-                            if (isDraw && ctx) {
-                                // Рисуем рамку
-                                ctx.lineWidth = 4;
-                                ctx.lineCap = 'square';
-                                ctx.lineJoin = 'bevel';
-                                ctx.strokeStyle = '#5199d9';
-
-
-                                //ctx.strokeRect(transformedX, y, width, height);
-                                ctx.strokeRect(transformedX, y, width, height);
-                            }
+                        let ctx: any = null
+                        if (isDraw) {
+                            ctx = canvas.getContext('2d');
+                            if (!ctx) return
+                            ctx.clearRect(0, 0, canvas.width, canvas.height);
                         }
-                    );
-                    setIsFaceDetected(faceDetected);
-                    if (isDraw && ctx) {
-                        ctx.beginPath();
-                        ctx.arc(circle.cx, circle.cy, circle.r, 0, 2 * Math.PI); // Рисуем круг
-                        ctx.fillStyle = faceDetected ? 'transparent' : 'transparent'; // Зеленый если лицо внутри круга, красный если нет
-                        ctx.fill();
-                        ctx.lineWidth = 4;
-                        ctx.strokeStyle = faceDetected ? 'green' : 'red';
-                        ctx.stroke();
-                    }
-                    console.log(i+') 888', new Date().toISOString());
-                    y++
 
+
+                        // Проверяем, попадает ли лицо в оверлей
+                        const circle: Circle = {cx: canvas.clientWidth / 2, cy: canvas.clientHeight / 2, r: 150}; // Создаем круг в центре видео с радиусом 150px
+
+                        let faceDetected = false// Флаг для проверки, было ли найдено лицо в области круга
+                        resizedDetections.forEach((detection) => {
+                                // Извлекаем информацию о прямоугольнике, в котором расположено лицо
+                                const {x, y, width, height} = detection.box;
+                                // Если применен scaleX(-1), нужно скорректировать координаты
+                                const transformedX = canvas.clientWidth - (x + width);
+                                //const rectangle: Rectangle = {x: transformedX, y, width, height};
+                                const rectangle: Rectangle = {x: transformedX, y, width, height}
+
+                                // Проверяем, перекрывает ли прямоугольник (лицо) круг более чем на 80%
+                                // И что бы круг был не больше чем в 5 раз больше прямоугольник , инчае слишко далеко лицо
+                                faceDetected = isRectangleCoveredByCircle(circle, rectangle, 0.8)
+                                if (isDraw && ctx) {
+                                    // Рисуем рамку
+                                    ctx.lineWidth = 4;
+                                    ctx.lineCap = 'square';
+                                    ctx.lineJoin = 'bevel';
+                                    ctx.strokeStyle = '#5199d9';
+
+
+                                    //ctx.strokeRect(transformedX, y, width, height);
+                                    ctx.strokeRect(transformedX, y, width, height);
+                                }
+                            }
+                        );
+                        setIsFaceDetected(faceDetected);
+                        if (isDraw && ctx) {
+                            ctx.beginPath();
+                            ctx.arc(circle.cx, circle.cy, circle.r, 0, 2 * Math.PI); // Рисуем круг
+                            ctx.fillStyle = faceDetected ? 'transparent' : 'transparent'; // Зеленый если лицо внутри круга, красный если нет
+                            ctx.fill();
+                            ctx.lineWidth = 4;
+                            ctx.strokeStyle = faceDetected ? 'green' : 'red';
+                            ctx.stroke();
+                        }
+                        console.log(i + ') 888', new Date().toISOString());
+
+
+                    }
+                } catch (error) {
+                    console.error('Ошибка в detectFace:', error);
+                } finally {
+                    // Запускаем следующий вызов через 100 мс после завершения
+                    timeoutId = setTimeout(detectFace, 100);
                 }
             };
-
-            interval = setInterval(detectFace, 100);// Запускаем детекцию лиц каждые 100 миллисекунд
+            detectFace()
         }
-        return () => clearInterval(interval); // Останавливаем детекцию при размонтировании компонента
-
-    }, [modelsLoaded, isCameraOn,isLoaded]);
+        return () => {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+        };
+    }, [modelsLoaded, isCameraOn, isLoaded]);
 
     return {isFaceDetected, modelsLoaded};
 };
