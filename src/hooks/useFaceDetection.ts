0c8a51c47c6d3dce75b1e44c83af48ccae94822f
veric <verich@mailru>
2025-06-09 17:06:27 +0300

    1

--- src/hooks/useFaceDetection.ts
+++ src/hooks/useFaceDetection.ts
@@ -19,7 +19,7 @@ const config: Partial<Config> = {
     body: {enabled: false}, // Отключаем детекцию тела
     hand: {enabled: false}, // Отключаем детекцию рук
     gesture: {enabled: false}, // Отключаем детекцию жестов
-    backend: "webgl", // Используем WebGL для ускорения (можно заменить на "wasm" или "cpu")
+    backend: "cpu", // Используем WebGL для ускорения (можно заменить на "wasm" или "cpu")
     cacheSensitivity: 0, // Отключаем кэширование для реального времени
 };
 
